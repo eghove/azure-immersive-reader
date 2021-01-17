9c879f5b3dd794269cf1cc51a37b668f2b5fc63d
Eric H <eghove@gmail.com>
2021-01-17 10:21:18 -0600

    setting up keys, .env

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TENANT_ID : process.env.TENANT_ID,
+    CLIENT_ID : process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    SUBDOMAIN : process.env.SUBDOMAIN
+}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-// const key = require("./keys");
+const keys = require("./keys");
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
