63e724b78fb5d7f0679e1d12cd29e41ceb7c4c0c
Eric H <eghove@gmail.com>
2021-01-23 11:50:30 -0600

    refactoring to use axios instead of request

--- api/services/azImmersiveReaderCredService.js
+++ api/services/azImmersiveReaderCredService.js
@@ -1,77 +1,52 @@
 const keys = require("../../keys");
-const request = require("request");
 const axios = require("axios");
+// Needed this `qs` package to make axios play well with urlencoded forms. See https://www.npmjs.com/package/axios#using-applicationx-www-form-urlencoded-format
 const qs = require("qs");
 
+// insuring all secrets are strings.
 const TENANT_ID = keys.TENANT_ID.toString();
 const CLIENT_ID = keys.CLIENT_ID.toString();
 const CLIENT_SECRET = keys.CLIENT_SECRET.toString();
 const SUBDOMAIN = keys.SUBDOMAIN.toString();
 
 module.exports = {
-    // development function to throw secrets back to make sure they are reading in correctly.
-    showSecrets: function (req, res) {
-        const secrets = [TENANT_ID, CLIENT_ID, CLIENT_SECRET, SUBDOMAIN];
-        res.json(secrets)
-    },
-    // Took this with superficial modications from: https://docs.microsoft.com/en-us/azure/cognitive-services/immersive-reader/quickstarts/client-libraries?pivots=programming-language-nodejs
-    getCredentials : function (req, res) {
-        const WINDOWS_LOGIN_URL =  `https://login.windows.net/${TENANT_ID}/oauth2/token`;
-        try {
-            request.post({
-                headers: {
-                    'content-type': 'application/x-www-form-urlencoded'
-                },
-                url: WINDOWS_LOGIN_URL,
-                form: {
-                    grant_type: 'client_credentials',
-                    client_id: CLIENT_ID,
-                    client_secret: CLIENT_SECRET,
-                    resource: 'https://cognitiveservices.azure.com/'
-                }
-            },
-            function(err, resp, tokenResult) {
-                if (err) {
-                    console.log(err);
-                    return res.status(500).send('CogSvcs IssueToken error');
-                }
-    
-                var tokenResultParsed = JSON.parse(tokenResult);
-    
-                if (tokenResultParsed.error) {
-                    console.log(tokenResult);
-                    return res.send({error :  "Unable to acquire Azure AD token. Check the debugger for more information."})
-                }
-    
-                var token = tokenResultParsed.access_token;
-                var subdomain = SUBDOMAIN;
-                return res.send({token, subdomain});
-            });
-        } catch (err) {
-            console.log(err);
-            return res.status(500).send('CogSvcs IssueToken error');
-        } 
-    },
-    // bringing back axios since request is deprecated.
-    getCredentialsAxios : function(req, res) {
-        const WINDOWS_LOGIN_URL =  `https://login.windows.net/${TENANT_ID}/oauth2/token`;
-        axios({
-            method: "post",
-            headers: {'content-type': 'application/x-www-form-urlencoded'},
-            url: WINDOWS_LOGIN_URL,
-            data: qs.stringify({
-                grant_type: 'client_credentials',
-                client_id: CLIENT_ID,
-                client_secret: CLIENT_SECRET,
-                resource: 'https://cognitiveservices.azure.com/'
-            })
-        })
-        .then(function (response){
-            res.json(response.data)
-        })
-        .catch((error) => {
-            res.status(422).json(error);
-        })
-    }
-
-}
+  // development function to throw secrets back to make sure they are reading in correctly.
+  showSecrets: function (req, res) {
+    const secrets = [TENANT_ID, CLIENT_ID, CLIENT_SECRET, SUBDOMAIN];
+    return res.json(secrets);
+  },
+  // Took this with modifications to use axios instead of deprecated package `request` from: https://docs.microsoft.com/en-us/azure/cognitive-services/immersive-reader/quickstarts/client-libraries?pivots=programming-language-nodejs
+  getCredentialsAxios: function (req, res) {
+    const WINDOWS_LOGIN_URL = `https://login.windows.net/${TENANT_ID}/oauth2/token`;
+    axios({
+      method: "post",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      url: WINDOWS_LOGIN_URL,
+      data: qs.stringify({
+        grant_type: "client_credentials",
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        resource: "https://cognitiveservices.azure.com/",
+      }),
+    })
+      .then(function (response) {
+        // parse the token
+        const accessToken = response.data.access_token;
+        if (accessToken !== null) {
+          const returnObject = {
+            access_token: accessToken,
+            subdomain: SUBDOMAIN,
+          };
+          return res.json(returnObject);
+        } else {
+          return res
+            .status(500)
+            .send("There was an error in parsing the access token.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        return res.status(500).send("CogSvcs IssueToken Error.");
+      });
+  },
+};
