d756ef577e722b03400b8aa0ac1ce19edd23f2d3
Eric H <eghove@gmail.com>
2021-01-23 11:52:56 -0600

    removing defunct routes

--- api/controllers/immersiveReaderController.js
+++ api/controllers/immersiveReaderController.js
@@ -2,13 +2,9 @@ const router = require("express").Router();
 const azImmersiveReaderCredServices = require("../services/azImmersiveReaderCredService");
 // ROUTE TO GET TOKEN AND SUBDOMAIN FOR THE AZ IMMERSIVE READER RESOURCE
 // ENDPOINT = "/api/azimmersivereader/gettokenandsubdomain"
-router.get('/gettokenandsubdomain', function (req, res){
-    // console.log('/gettokenandsubdomain called');
-    // res.send("This worked.");
-    //azImmersiveReaderCredServices.showSecrets(req, res);
-    //azImmersiveReaderCredServices.getCredentials(req, res);
-    azImmersiveReaderCredServices.getCredentialsAxios(req, res);
-
+router.get("/gettokenandsubdomain", function (req, res) {
+  //azImmersiveReaderCredServices.showSecrets(req, res);
+  azImmersiveReaderCredServices.getCredentialsAxios(req, res);
 });
 
 module.exports = router;
