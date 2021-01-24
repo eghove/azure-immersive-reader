4cf93d15821897ac8fe475d51c95cf18c6115c38
Eric H <eghove@gmail.com>
2021-01-24 12:42:19 -0600

    initial dummy content for HomePage

--- client/src/components/Pages/Home/HomePage.tsx
+++ client/src/components/Pages/Home/HomePage.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+// page content import
+import homePageContent from "./homePageContent";
+// react-bootstrap imports
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Card from "react-bootstrap/Card";
 
 const HomePage = () => {
   return (
     <React.Fragment>
-      <div>This is Home Page.</div>
+      <Row>
+        <Col>
+          <Jumbotron>
+            <h1>{homePageContent.jumboText}</h1>
+            <p>{homePageContent.sloganText}</p>
+          </Jumbotron>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <p>{homePageContent.introText1}</p>
+              <p>{homePageContent.introText2}</p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </React.Fragment>
   );
 };
---
+++ client/src/components/Pages/Home/homePageContent.tsx
@@ -0,0 +1,10 @@
+const homePageContent = {
+  jumboText: "Welcome to Hove's Stoves!",
+  sloganText: "Saint Paul's choice for quality vintage stoves since 2021!",
+  introText1:
+    "Here at Hove's Stoves, we specialize in the finest quality vintage stoves. Family-owned since 2021, we pride ourselves on great products, reasonable prices, and exceptional service!",
+  introText2:
+    "Browse our website for our available stock, or contact us directly. We'd be thrilled to help you find the quality vintage stove of your dreams, today!",
+};
+
+export default homePageContent;
