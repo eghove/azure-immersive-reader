958c0fd9b925313bd7cb94adb4ccf3491e4f2bc0
Eric H <eghove@gmail.com>
2021-01-31 10:41:55 -0600

    scaffold layout for product page

--- client/src/components/Pages/Products/ProductsPage.tsx
+++ client/src/components/Pages/Products/ProductsPage.tsx
@@ -1,9 +1,57 @@
 import React from "react";
+import ImmersiveReader from "../Shared/ImmersiveReader";
+// react-bootstrap imports
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Card from "react-bootstrap/Card";
 
 const ProductsPage = () => {
   return (
     <React.Fragment>
-      <div>This is Products Page.</div>
+      <Row>
+        <Col>
+          <Jumbotron>
+            <h1>Products</h1>
+            <p>Check out our "hottest" vintage stoves!</p>
+          </Jumbotron>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Big Red</Card.Title>
+              <Card.Text>
+                This is a classic red stove, sure to be a coversation starter in
+                your home!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Big Red</Card.Title>
+              <Card.Text>
+                This is a classic red stove, sure to be a coversation starter in
+                your home!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Big Red</Card.Title>
+              <Card.Text>
+                This is a classic red stove, sure to be a coversation starter in
+                your home!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </React.Fragment>
   );
 };
