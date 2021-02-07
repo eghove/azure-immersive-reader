ad3406a4ea4d9279693b6dcecd630d183c8b4651
Eric H <eghove@gmail.com>
2021-02-07 14:20:48 -0600

    light styling on not found page

--- client/src/components/Pages/NotFound/NotFoundPage.tsx
+++ client/src/components/Pages/NotFound/NotFoundPage.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+// react-bootstrap imports
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const NotFoundPage = () => {
   return (
     <React.Fragment>
-      <div>This is not found Page.</div>
+      <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          <Card>
+            <Card.Body>
+              <p>
+                The page you requested was not found. Please select one of the
+                options in the navigation bar.
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </React.Fragment>
   );
 };
