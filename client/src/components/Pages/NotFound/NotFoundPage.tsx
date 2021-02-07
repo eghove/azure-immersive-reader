7f38185a7f63f69a4e040eb9afbdcbe8bca26b86
Eric H <eghove@gmail.com>
2021-02-07 14:25:34 -0600

    little dynamic content in notfoundpage

--- client/src/components/Pages/NotFound/NotFoundPage.tsx
+++ client/src/components/Pages/NotFound/NotFoundPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 // react-bootstrap imports
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
 const NotFoundPage = () => {
+  let location = useLocation();
   return (
     <React.Fragment>
       <Row>
@@ -12,8 +14,8 @@ const NotFoundPage = () => {
           <Card>
             <Card.Body>
               <p>
-                The page you requested was not found. Please select one of the
-                options in the navigation bar.
+                The page you requested using {location.pathname} was not found.
+                Please select one of the options in the navigation bar.
               </p>
             </Card.Body>
           </Card>
